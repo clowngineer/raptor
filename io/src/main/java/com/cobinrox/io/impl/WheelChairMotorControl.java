20f27716faee15657252528980dbd6a9d7ae2411
CJ Rox <clowngineer@yahoo.com>
2015-05-08 16:36:11 -0600

    encode for wheelchair motors

--- io/src/main/java/com/cobinrox/io/impl/WheelChairMotorControl.java
+++ io/src/main/java/com/cobinrox/io/impl/WheelChairMotorControl.java
@@ -33,8 +33,12 @@ public class WheelChairMotorControl implements IMotorControl {
 		public void brakeAll() throws Throwable{myMotors.brakeAll();}
 	*/
 	
-	String lrState;
-	String fbState;
+	String m2lrState;
+	String m1fbState;
+	String m1;
+	String m2;
+	//String m1State;
+	//String m2State;
 	public void move( String foreAft,  String leftRight) {
 		// first check what the CALLER sent in . . .
 		/*if( foreAft != null && foreAft.length() > 2 )
@@ -47,24 +51,30 @@ public class WheelChairMotorControl implements IMotorControl {
 			logger.info("Not sure how to handle leftRight of [" + leftRight + "]");
 			return;
 		}*/
+		m1 = null;
+		m2 = null;
 		if( foreAft != null && leftRight != null )
 		{
 			logger.info("Not sure how to handle both foreAft & leftRight");
 			return;
 		}
 		
-		// now re-code to remote car motor command
+		// now re-code for m1 and m2 wheelchair motor mode
 		if( foreAft != null )
 		{
 			if( foreAft.equals(MotorProps.FORWARD))
 			{
 				// we want both motors to move +
 				leftRight = MotorProps.LEFT;
+				m1 = "+";
+				m2 = "+";
 			}
-			else
+			else if( foreAft.equals(MotorProps.BACKWARD))
 			{
 				// we want both motors to move -
 				leftRight = MotorProps.RIGHT;
+				m1 = "-";
+				m2 = "-";
 			}
 		}
 		else
@@ -72,21 +82,19 @@ public class WheelChairMotorControl implements IMotorControl {
 			if( leftRight.equals(MotorProps.LEFT))
 			{
 				foreAft = MotorProps.BACKWARD;
-				//foreAft   = minus;
-				//leftRight = plus;
+				m1 = "-";
+				m2 = "+";
 			}
 			else if( leftRight.equals(MotorProps.RIGHT))
 			{
-				leftRight = MotorProps.LEFT;
 				foreAft = MotorProps.FORWARD;
-				//foreAft = minus;
-				//leftRight = minus;
+				m1="+";
+				m2="-";
 			}
 		}
-		
-		    		
-		logger.info("About to move [" + foreAft + "/" + leftRight
-				+ "]...");
+
+		logger.info("About to move [" + foreAft + "/" + leftRight+ "]...");
+		logger.info("About to move m1 [" + m1 + "] m2 [" + m2 + "]");
 		if( mp == null )
 		{
 			logger.error("INVALID STATE MP NOT SET");
@@ -97,46 +105,48 @@ public class WheelChairMotorControl implements IMotorControl {
 			mp=new MotorProps();
 			mp.setProps(false);
 		}*/
-		lrState = "processing";
-		fbState = "processing";
+		m2lrState =  "processing";
+		m1fbState =  "processing";
 		try {
-			Thread fbThread = null;
-			Thread lrThread = null;
+			Thread m1fbThread = null;
+			Thread m2lrThread = null;
 			if (foreAft != null) {
-				final String foreAftT = foreAft;
-				fbThread = new Thread() {
+				final String m1foreAftT = foreAft;
+				m1fbThread = new Thread() {
 					public void run() {
-						pulse(foreAftT, null,mp.DUTY_CYCLE_HI_MS, mp.DUTY_CYCLE_LO_MS, mp.CMD_RUN_TIME_MS);
-						fbState=null;
+						pulse(m1foreAftT, null,mp.DUTY_CYCLE_HI_MS, mp.DUTY_CYCLE_LO_MS, mp.CMD_RUN_TIME_MS);
+						//pulse(m1, null,mp.DUTY_CYCLE_HI_MS, mp.DUTY_CYCLE_LO_MS, mp.CMD_RUN_TIME_MS);
+						m1fbState=null;
 					}
 				};
-				fbThread.setName("FWD_THREAD");
+				m1fbThread.setName("M1FB_THREAD");
 			}
 			if (leftRight != null) {
-				final String leftRightT = leftRight;
-				lrThread = new Thread() {
+				final String m2leftRightT = leftRight;
+				m2lrThread = new Thread() {
 					public void run() {
-						pulse(null,leftRightT, mp.DUTY_CYCLE_HI_MS, mp.DUTY_CYCLE_LO_MS, mp.CMD_RUN_TIME_MS);
-						lrState = null;
+						pulse(null,m2leftRightT, mp.DUTY_CYCLE_HI_MS, mp.DUTY_CYCLE_LO_MS, mp.CMD_RUN_TIME_MS);
+						//pulse(null,m2, mp.DUTY_CYCLE_HI_MS, mp.DUTY_CYCLE_LO_MS, mp.CMD_RUN_TIME_MS);
+						m2lrState = null;
 					}
 				};
-				lrThread.setName("LR_THREAD");
+				m2lrThread.setName("M2LR_THREAD");
 			}
-			if (leftRight != null)
-				lrThread.start();
-			if (foreAft != null)
-				fbThread.start();
+			if (m2 != null) //leftRight != null)
+				m2lrThread.start();
+			if (m1 != null) //(foreAft != null)
+				m1fbThread.start();
 			// if(lrThread  == null &&  (fwdThread!=null && !forwardBackPulseComplete))
 			// if(fwdThread == null &&  (lrThead!=null   && !leftRightPulseComplete  )) 
 			// 
-			while (lrThread != null && lrState != null)
+			while (m2lrThread != null && m2lrState != null)
 			{
-				logger.info("......................lrState/" + lrState);
+				logger.info("......................m2lrState/" + m2lrState);
 				Thread.sleep(200);
 			}
-			while (fbThread != null && fbState != null)
+			while (m1fbThread != null && m1fbState != null)
 			{
-				logger.info("......................fbState/" + fbState);
+				logger.info("......................m1fbState/" + m1fbState);
 				Thread.sleep(200);
 			}
 			myMotors.brakeAll();
@@ -147,7 +157,7 @@ public class WheelChairMotorControl implements IMotorControl {
 		}
 	} 
 	
-	protected void pulse(final String foreAft, final String leftRight, int dutyCycleHi, int dutyCycleLo,
+	protected void pulse(final String m1foreAft, final String m2leftRight, int dutyCycleHi, int dutyCycleLo,
 			int numMsToRunCmdFor) {
 		int numMsHi = dutyCycleHi;//dutyCycleHi;//(int)((float)numMsToRunCmdFor * (float)((float)dutyCycleHi/(float)100));
 		int numMsLo = dutyCycleLo;
@@ -160,13 +170,13 @@ public class WheelChairMotorControl implements IMotorControl {
 				for (int i = 0; i < numPeriods; i++) {
 					long time = System.currentTimeMillis();
 					logger.info("     turning on for [" + numMsHi + "]ms...");
-					myMotors.lowLevelMove(foreAft,leftRight);
+					myMotors.lowLevelMove(m1foreAft,m2leftRight);
 					Thread.sleep(numMsHi);
 
 					if (numMsLo > 0) {
 						time = System.currentTimeMillis();
 						logger.info("     turning off for [" + numMsLo + "]ms...");
-						myMotors.lowLevelStop(foreAft,leftRight);
+						myMotors.lowLevelStop(m1foreAft,m2leftRight);
 						Thread.sleep(numMsLo);
 					}
 				}
